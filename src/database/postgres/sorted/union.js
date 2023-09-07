dbc484077a7173134b5351bba013f988bd6ae768
Eichel Choi <eichel@eichels-mbp-3.wifi.local.cmu.edu>
2023-09-07 01:49:57 -0400

    Run Test1 for union.ts

--- src/database/postgres/sorted/union.js
+++ src/database/postgres/sorted/union.js
@@ -1,57 +1,65 @@
 'use strict';
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 module.exports = function (module) {
-    module.sortedSetUnionCard = async function (keys) {
-        if (!Array.isArray(keys) || !keys.length) {
-            return 0;
-        }
-
-        const res = await module.pool.query({
-            name: 'sortedSetUnionCard',
-            text: `
+    module.sortedSetUnionCard = function (keys) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!Array.isArray(keys) || !keys.length) {
+                return 0;
+            }
+            const res = yield module.pool.query({
+                name: 'sortedSetUnionCard',
+                text: `
 SELECT COUNT(DISTINCT z."value") c
   FROM "legacy_object_live" o
  INNER JOIN "legacy_zset" z
          ON o."_key" = z."_key"
         AND o."type" = z."type"
  WHERE o."_key" = ANY($1::TEXT[])`,
-            values: [keys],
+                values: [keys],
+            });
+            return res.rows[0].c;
         });
-        return res.rows[0].c;
     };
-
-    module.getSortedSetUnion = async function (params) {
-        params.sort = 1;
-        return await getSortedSetUnion(params);
+    module.getSortedSetUnion = function (params) {
+        return __awaiter(this, void 0, void 0, function* () {
+            params.sort = 1;
+            return yield getSortedSetUnion(params);
+        });
     };
-
-    module.getSortedSetRevUnion = async function (params) {
-        params.sort = -1;
-        return await getSortedSetUnion(params);
+    module.getSortedSetRevUnion = function (params) {
+        return __awaiter(this, void 0, void 0, function* () {
+            params.sort = -1;
+            return yield getSortedSetUnion(params);
+        });
     };
-
-    async function getSortedSetUnion(params) {
-        const { sets } = params;
-        const start = params.hasOwnProperty('start') ? params.start : 0;
-        const stop = params.hasOwnProperty('stop') ? params.stop : -1;
-        let weights = params.weights || [];
-        const aggregate = params.aggregate || 'SUM';
-
-        if (sets.length < weights.length) {
-            weights = weights.slice(0, sets.length);
-        }
-        while (sets.length > weights.length) {
-            weights.push(1);
-        }
-
-        let limit = stop - start + 1;
-        if (limit <= 0) {
-            limit = null;
-        }
-
-        const res = await module.pool.query({
-            name: `getSortedSetUnion${aggregate}${params.sort > 0 ? 'Asc' : 'Desc'}WithScores`,
-            text: `
+    function getSortedSetUnion(params) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { sets } = params;
+            const start = params.hasOwnProperty('start') ? params.start : 0;
+            const stop = params.hasOwnProperty('stop') ? params.stop : -1;
+            let weights = params.weights || [];
+            const aggregate = params.aggregate || 'SUM';
+            if (sets.length < weights.length) {
+                weights = weights.slice(0, sets.length);
+            }
+            while (sets.length > weights.length) {
+                weights.push(1);
+            }
+            let limit = stop - start + 1;
+            if (limit <= 0) {
+                limit = null;
+            }
+            const res = yield module.pool.query({
+                name: `getSortedSetUnion${aggregate}${params.sort > 0 ? 'Asc' : 'Desc'}WithScores`,
+                text: `
 WITH A AS (SELECT z."value",
                   ${aggregate}(z."score" * k."weight") "score"
              FROM UNNEST($1::TEXT[], $2::NUMERIC[]) k("_key", "weight")
@@ -67,17 +75,18 @@ SELECT A."value",
  ORDER BY A."score" ${params.sort > 0 ? 'ASC' : 'DESC'}
  LIMIT $4::INTEGER
 OFFSET $3::INTEGER`,
-            values: [sets, weights, start, limit],
+                values: [sets, weights, start, limit],
+            });
+            if (params.withScores) {
+                res.rows = res.rows.map((r) => ({
+                    value: r.value,
+                    score: parseFloat(r.score),
+                }));
+            }
+            else {
+                res.rows = res.rows.map((r) => r.value);
+            }
+            return res.rows;
         });
-
-        if (params.withScores) {
-            res.rows = res.rows.map(r => ({
-                value: r.value,
-                score: parseFloat(r.score),
-            }));
-        } else {
-            res.rows = res.rows.map(r => r.value);
-        }
-        return res.rows;
     }
 };
